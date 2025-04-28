1089dcbfcf8b39407fcd6e40825143cc4ad7852b
sandeepmirashi <sandeep.sm83@gmail.com>
2025-04-28 11:36:06 +0530

    Initial commit

---
+++ src/main/java/com/mirashitech/playjms/PlayjmsApplication.java
@@ -0,0 +1,13 @@
+package com.mirashitech.playjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayjmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayjmsApplication.class, args);
+	}
+
+}
